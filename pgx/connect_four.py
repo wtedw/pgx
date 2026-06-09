367e174a2e67f5cfd524c6cd5169a60fcea3fce0
ted <wtedw@yahoo.com>
2026-06-09 13:04:07 +0900

    connect4 dont randomize current player

--- pgx/connect_four.py
+++ pgx/connect_four.py
@@ -43,7 +43,7 @@ class ConnectFour(core.Env):
         self._game = Game()
 
     def _init(self, key: PRNGKey) -> State:
-        current_player = jnp.int32(jax.random.bernoulli(key))
+        current_player = jnp.int32(0)  # First player always starts
         return State(current_player=current_player, _x=self._game.init())  # type:ignore
 
     def _step(self, state: core.State, action: Array, key) -> State:
